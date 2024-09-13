66d118fb02b922bb47e19023b96ded97444cec33
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-09-13 11:53:07 +0200

    added max-height to my exam card

--- src/components/cards/StyledCard.styles.jsx
+++ src/components/cards/StyledCard.styles.jsx
@@ -20,7 +20,7 @@ export const StyledExamCard = styled.div`
    border-color: 2px black;
    padding: 60px;
    max-width: 600px;
-   height: 500px;
+   max-height: 1000px;
 
    img {
      width: 100%;
