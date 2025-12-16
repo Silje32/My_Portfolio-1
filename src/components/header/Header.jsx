1d4267d4bef422929094876fc81b63190b806091
Silje32 <msh070425@gmail.com>
2025-12-16 12:48:28 +0100

    Removed my styled components styling and added css.modules styling

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,11 @@
-import { StyledHeading } from "./Heading.styles";
+import styles from "./Heading.module.css";
 
 function Header() {
-
-    return (
-      <>
-        <StyledHeading>
-             PORTFOLIO 1
-        </StyledHeading>
-      </>     
-    );
+  return (
+    <div className={styles.heading}>
+      <h1>PORTFOLIO 1</h1>
+    </div>
+  );
 }
 
 export default Header;
