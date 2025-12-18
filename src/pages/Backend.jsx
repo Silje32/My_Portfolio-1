1818e10af835ae883cfd4d4b5cfca93741757e39
Silje32 <msh070425@gmail.com>
2025-12-18 09:56:47 +0100

    Fixed a bug and removed my styled components styling

--- src/pages/Backend.jsx
+++ src/pages/Backend.jsx
@@ -1,16 +1,13 @@
 import ExamCard from "../components/cards/ExamCard";
 import SemesterProjectCard from "../components/cards/SemesterProjectCard";
-import LeathaCard from "../components/cards/LethaCard";
-import { StyledCard } from "../components/cards/StyledCard.styles";
+import LethaCard from "../components/cards/LethaCard";
 
 function Backend() {
   return (
     <>
-      <StyledCard>
-        <ExamCard />
-        <SemesterProjectCard />
-        <LeathaCard />
-      </StyledCard>
+      <ExamCard />
+      <SemesterProjectCard />
+      <LethaCard />
     </>
   );
 }
