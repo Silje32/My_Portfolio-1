f42070cb645bfe646330d65e23651bc5946b41f6
Silje32 <msh070425@gmail.com>
2025-12-25 21:17:57 +0100

    Added my CsharpCard and styled it

---
+++ src/components/cards/CsharpCard.jsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+import styles from "../cards/StyledCard.module.css";
+
+function CsharpCard() {
+  return (
+    <>
+      <div className={styles.csharpCard}>
+        <div>
+          <h2 className={styles.heading}>MY FIRST LIBRARY:</h2>
+        </div>
+        <div>
+          <p>
+            Imagine that you work at a library and people are there to loan
+            books from you. Create an application that manages the loans of the
+            books.
+          </p>
+        </div>
+        <Link
+          className={styles.link}
+          to={"https://github.com/Silje32/Library.git"}
+        >
+          VIEW GITHUB REPOSITORY
+        </Link>
+      </div>
+    </>
+  );
+}
+
+export default CsharpCard;
