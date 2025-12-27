63013c7a1feb35df763f61ff2f6d10e79349f724
Silje32 <msh070425@gmail.com>
2025-12-27 21:59:24 +0100

    Made space for my future next project

---
+++ src/components/cards/NextCard.jsx
@@ -0,0 +1,18 @@
+import styles from "../cards/StyledCard.module.css";
+
+function NextCard() {
+  return (
+    <>
+      <div className={styles.NextCard}>
+        <div>
+          <h2 className={styles.heading}>NEXT.JS PROJECT</h2>
+        </div>
+        <div>
+          <p>My First Next.js Project.</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default NextCard;
