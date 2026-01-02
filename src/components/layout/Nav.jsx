a6145c5b73cb6ae1dc02ff2569fdf5e36ba8c742
Silje32 <msh070425@gmail.com>
2026-01-02 14:30:55 +0100

    Changed my NavLink to Link

--- src/components/layout/Nav.jsx
+++ src/components/layout/Nav.jsx
@@ -1,16 +1,16 @@
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import styles from "./Nav.module.css";
 
 function Nav() {
   return (
     <>
       <nav>
-        <NavLink to="/" className={styles.navlink}>
+        <Link to="/" className={styles.navlink}>
           Frontend Projects
-        </NavLink>
-        <NavLink to="/backend" className={styles.navlink}>
+        </Link>
+        <Link to="/backend" className={styles.navlink}>
           Backend Projects
-        </NavLink>
+        </Link>
       </nav>
     </>
   );
