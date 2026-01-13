973285359f10ecd9481e01b1c7de39c60c68ee15
Silje32 <msh070425@gmail.com>
2026-01-13 14:50:23 +0100

    Implemented my ExamCard to my FrontendCard and deleted my ExamCard

--- src/components/cards/MyFrontendCard.jsx
+++ src/components/cards/MyFrontendCard.jsx
@@ -1,9 +1,40 @@
+import { Link } from "react-router-dom";
+import Screenshot from "../../images/website_project-exam.png";
+import ExamButton from "../buttons/ExamButton";
 import styles from "./StyledCard.module.css";
 
 function MyFrontendCard() {
   return (
     <div className={styles.container}>
-      <div className={"${styles.item} ${styles.ExamCard}"}>Project Exam 1</div>
+      <div>
+        <div className={"${styles.item} ${styles.ExamCard}"}>
+          Rovers on Mars
+        </div>
+        <div>
+          <h2 className="h2">PROJECT EXAM 1:</h2>
+        </div>
+
+        <div>
+          <img src={Screenshot} alt="Screenshot" />
+        </div>
+        <div>
+          <p>
+            Create a microsite for SpaceX / NASA that focuses on space
+            technology to raise awareness about space program activity around
+            the world.
+          </p>
+        </div>
+        <Link
+          className={styles.link}
+          to={"https://github.com/Silje32/Project_Exam_SpaceX"}
+        >
+          VIEW GITHUB REPOSITORY
+        </Link>
+        <div>
+          <ExamButton />
+        </div>
+      </div>
+
       <div className={styles.item}>Semester Project 1</div>
       <div className={styles.item}>Interaction Design CA</div>
     </div>
