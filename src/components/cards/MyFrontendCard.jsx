4856b195e35efe1bb71a60d0f60c6db2ed1acea7
Silje32 <msh070425@gmail.com>
2026-01-14 10:14:21 +0100

    Implemented my SemesterProjectCard in my Frontend card and Increased the size of my screenshots

--- src/components/cards/MyFrontendCard.jsx
+++ src/components/cards/MyFrontendCard.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
-import Screenshot from "../../images/website_project-exam.png";
+import Screenshot1 from "../../images/website_project-exam.png";
+import Screenshot2 from "../../images/website_semester-project1.png";
 import ExamButton from "../buttons/ExamButton";
+import ProjectButton from "../buttons/ProjectButton";
 import styles from "./StyledCard.module.css";
 
 function MyFrontendCard() {
@@ -15,7 +17,7 @@ function MyFrontendCard() {
         </div>
 
         <div>
-          <img src={Screenshot} alt="Screenshot" />
+          <img src={Screenshot1} alt="Screenshot" />
         </div>
         <div>
           <p>
@@ -34,8 +36,38 @@ function MyFrontendCard() {
           <ExamButton />
         </div>
       </div>
-
-      <div className={styles.item}>Semester Project 1</div>
+      <div className={"${styles.item} ${styles.SemesterProjectCard}"}>
+        Community Science Museum
+      </div>
+      <div>
+        <h2 className="h2">SEMESTER PROJECT 1:</h2>
+      </div>
+      <div>
+        <img src={Screenshot2} alt="Screenshot" />
+      </div>
+      <div>
+        <p>
+          An interactive science museum called the Community Science Museum is
+          opening in your town/city. Its core target audience is primary and
+          middle school children (7-15) and families with young children. The
+          museum wants a website that is informative and appealing, that will
+          attract both middle school pupils and their parents. The site should
+          appeal to youngsters without pandering; it should take for granted
+          that the audience is inquisitive and intelligent. The website should
+          be informative and engaging, but should encourage viewers to visit the
+          museum itself. The website should be responsive and be easy to use on
+          a variety of devices.
+        </p>
+      </div>
+      <Link
+        className={styles.link}
+        to={"https://github.com/Silje32/Semester_project.git"}
+      >
+        VIEW GITHUB REPOSITORY
+      </Link>
+      <div>
+        <ProjectButton />
+      </div>
       <div className={styles.item}>Interaction Design CA</div>
     </div>
   );
