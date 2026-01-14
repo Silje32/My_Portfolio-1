1e874b2178c5d4f758547af0ed428519d854a325
Silje32 <msh070425@gmail.com>
2026-01-14 13:57:42 +0100

    Added a couple of divs and capsulated each card so my flex could work

--- src/components/cards/MyFrontendCard.jsx
+++ src/components/cards/MyFrontendCard.jsx
@@ -11,9 +11,7 @@ function MyFrontendCard() {
   return (
     <div className={styles.container}>
       <div>
-        <div className={"${styles.item} ${styles.ExamCard}"}>
-          Rovers on Mars
-        </div>
+        <div className={"${styles.item} ${styles.Card}"}></div>
         <div>
           <h2 className="h2">PROJECT EXAM 1:</h2>
         </div>
@@ -38,61 +36,61 @@ function MyFrontendCard() {
           <ExamButton />
         </div>
       </div>
-      <div className={"${styles.item} ${styles.SemesterProjectCard}"}>
-        Community Science Museum
+      <div>
+        <div className={"${styles.item} ${styles.Card}"}></div>
+        <div>
+          <h2 className="h2">SEMESTER PROJECT 1:</h2>
+        </div>
+        <div>
+          <img src={Screenshot2} alt="Screenshot" />
+        </div>
+        <div>
+          <p>
+            An interactive science museum called the Community Science Museum is
+            opening in your town/city. Its core target audience is primary and
+            middle school children (7-15) and families with young children. The
+            museum wants a website that is informative and appealing, that will
+            attract both middle school pupils and their parents. The site should
+            appeal to youngsters without pandering; it should take for granted
+            that the audience is inquisitive and intelligent. The website should
+            be informative and engaging, but should encourage viewers to visit
+            the museum itself. The website should be responsive and be easy to
+            use on a variety of devices.
+          </p>
+        </div>
+        <Link
+          className={styles.link}
+          to={"https://github.com/Silje32/Semester_project.git"}
+        >
+          VIEW GITHUB REPOSITORY
+        </Link>
+        <div>
+          <ProjectButton />
+        </div>
       </div>
       <div>
-        <h2 className="h2">SEMESTER PROJECT 1:</h2>
-      </div>
-      <div>
-        <img src={Screenshot2} alt="Screenshot" />
-      </div>
-      <div>
-        <p>
-          An interactive science museum called the Community Science Museum is
-          opening in your town/city. Its core target audience is primary and
-          middle school children (7-15) and families with young children. The
-          museum wants a website that is informative and appealing, that will
-          attract both middle school pupils and their parents. The site should
-          appeal to youngsters without pandering; it should take for granted
-          that the audience is inquisitive and intelligent. The website should
-          be informative and engaging, but should encourage viewers to visit the
-          museum itself. The website should be responsive and be easy to use on
-          a variety of devices.
-        </p>
-      </div>
-      <Link
-        className={styles.link}
-        to={"https://github.com/Silje32/Semester_project.git"}
-      >
-        VIEW GITHUB REPOSITORY
-      </Link>
-      <div>
-        <ProjectButton />
-      </div>
-      <div className={"${styles.item} ${styles.LethaCard }"}>
-        Interaction Design CA
-      </div>
-      <div>
-        <h2 className="h2">INTERACTION DESIGN CA:</h2>
-      </div>
-      <div>
-        <img src={Screenshot3} alt="Screenshot" />
-      </div>
-      <div>
-        <p>
-          Create the final version of the Letha website and upload it to your
-          web host.
-        </p>
-      </div>
-      <Link
-        className={styles.link}
-        to={"https://github.com/Silje32/Leatha_CA.git"}
-      >
-        VIEW GITHUB REPOSITORY
-      </Link>
-      <div>
-        <LethaButton />
+        <div className={"${styles.item} ${styles.Card}"}></div>
+        <div>
+          <h2 className="h2">INTERACTION DESIGN CA:</h2>
+        </div>
+        <div>
+          <img src={Screenshot3} alt="Screenshot" />
+        </div>
+        <div>
+          <p>
+            Create the final version of the Letha website and upload it to your
+            web host.
+          </p>
+        </div>
+        <Link
+          className={styles.link}
+          to={"https://github.com/Silje32/Leatha_CA.git"}
+        >
+          VIEW GITHUB REPOSITORY
+        </Link>
+        <div>
+          <LethaButton />
+        </div>
       </div>
     </div>
   );
