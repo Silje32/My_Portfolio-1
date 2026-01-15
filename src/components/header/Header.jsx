7e85b62b94aa62178f0ddf27f64a3c87368470bc
Silje32 <msh070425@gmail.com>
2026-01-15 10:47:52 +0100

    Added fragments to my Header function, margin-bottom to my h1 and decreased my h2 font-size.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,9 +2,11 @@ import styles from "./Heading.module.css";
 
 function Header() {
   return (
-    <div className={styles.heading}>
-      <h1>PORTFOLIO 1</h1>
-    </div>
+    <>
+      <div className={styles.heading}>
+        <h1>PORTFOLIO 1</h1>
+      </div>
+    </>
   );
 }
 
