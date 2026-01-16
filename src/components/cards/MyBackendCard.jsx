90be7e514ca813e9e060586fb3fd4c0ee9eb1e73
Silje32 <msh070425@gmail.com>
2026-01-16 09:40:02 +0100

    Moved my NodeCard to my Backendcard and deleted my NodeCard

--- src/components/cards/MyBackendCard.jsx
+++ src/components/cards/MyBackendCard.jsx
@@ -1,11 +1,26 @@
+import { Link } from "react-router-dom";
 import styles from "./StyledCard.module.css";
 
 function MyBackendCard() {
   return (
     <div className={styles.container}>
-      <div className={styles.item}>Node.JS Project</div>
-      <div className={styles.item}>Next.JS Project</div>
-      <div className={styles.item}>Csharp Project</div>
+      <div className={`${styles.item} ${styles.Card}`}>
+        <div>
+          <h2 className="h2">MOVIE REVIEW API:</h2>
+        </div>
+        <div>
+          <p>
+            Create a Movie Review API so movie lovers and critics can find,
+            share and review movies.
+          </p>
+        </div>
+        <Link
+          className={styles.link}
+          to={"https://github.com/Silje32/Movie_Review_Api.git"}
+        >
+          VIEW GITHUB REPOSITORY
+        </Link>
+      </div>
     </div>
   );
 }
