5445b7615b53190a46c9d9ec962151009ef97f18
Silje32 <msh070425@gmail.com>
2026-01-16 09:54:26 +0100

    Moved my CsharpCard to my Backendcard and deleted my CsharpCard

--- src/components/cards/MyBackendCard.jsx
+++ src/components/cards/MyBackendCard.jsx
@@ -3,25 +3,46 @@ import styles from "./StyledCard.module.css";
 
 function MyBackendCard() {
   return (
-    <div className={styles.container}>
+    <>
+      <div className={styles.container}>
+        <div className={`${styles.item} ${styles.Card}`}>
+          <div>
+            <h2 className="h2">MOVIE REVIEW API:</h2>
+          </div>
+          <div>
+            <p>
+              Create a Movie Review API so movie lovers and critics can find,
+              share and review movies.
+            </p>
+          </div>
+          <Link
+            className={styles.link}
+            to={"https://github.com/Silje32/Movie_Review_Api.git"}
+          >
+            VIEW GITHUB REPOSITORY
+          </Link>
+        </div>
+      </div>
+
       <div className={`${styles.item} ${styles.Card}`}>
         <div>
-          <h2 className="h2">MOVIE REVIEW API:</h2>
+          <h2 className="h2">MY FIRST LIBRARY:</h2>
         </div>
         <div>
           <p>
-            Create a Movie Review API so movie lovers and critics can find,
-            share and review movies.
+            Imagine that you work at a library and people are there to loan
+            books from you. Create an application that manages the loans of the
+            books.
           </p>
         </div>
         <Link
           className={styles.link}
-          to={"https://github.com/Silje32/Movie_Review_Api.git"}
+          to={"https://github.com/Silje32/Library.git"}
         >
           VIEW GITHUB REPOSITORY
         </Link>
       </div>
-    </div>
+    </>
   );
 }
 export default MyBackendCard;
