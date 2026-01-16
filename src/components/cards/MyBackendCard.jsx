4f764fe80815d54178b112ea716ec94a7a97a230
Silje32 <msh070425@gmail.com>
2026-01-16 10:13:22 +0100

    Moved my NextCard to my backendcard and deleted the NextCard

--- src/components/cards/MyBackendCard.jsx
+++ src/components/cards/MyBackendCard.jsx
@@ -22,25 +22,34 @@ function MyBackendCard() {
             VIEW GITHUB REPOSITORY
           </Link>
         </div>
-      </div>
 
-      <div className={`${styles.item} ${styles.Card}`}>
-        <div>
-          <h2 className="h2">MY FIRST LIBRARY:</h2>
+        <div className={`${styles.item} ${styles.Card}`}>
+          <div>
+            <h2 className="h2">MY FIRST LIBRARY:</h2>
+          </div>
+          <div>
+            <p>
+              Imagine that you work at a library and people are there to loan
+              books from you. Create an application that manages the loans of
+              the books.
+            </p>
+          </div>
+          <Link
+            className={styles.link}
+            to={"https://github.com/Silje32/Library.git"}
+          >
+            VIEW GITHUB REPOSITORY
+          </Link>
         </div>
-        <div>
-          <p>
-            Imagine that you work at a library and people are there to loan
-            books from you. Create an application that manages the loans of the
-            books.
-          </p>
+
+        <div className={`${styles.item} ${styles.Card}`}>
+          <div>
+            <h2 className="h2">NEXT.JS PROJECT</h2>
+          </div>
+          <div>
+            <p>My First Next.js Project.</p>
+          </div>
         </div>
-        <Link
-          className={styles.link}
-          to={"https://github.com/Silje32/Library.git"}
-        >
-          VIEW GITHUB REPOSITORY
-        </Link>
       </div>
     </>
   );
