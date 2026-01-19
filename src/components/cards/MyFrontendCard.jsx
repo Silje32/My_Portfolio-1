a65bcc83d59c7fd25feacfcf17403065110a9314
Silje32 <msh070425@gmail.com>
2026-01-19 14:26:19 +0100

    Changed my semesterproject card to react card

--- src/components/cards/MyFrontendCard.jsx
+++ src/components/cards/MyFrontendCard.jsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
 import Screenshot1 from "../../images/website-dom-manipulation.png";
-import Screenshot2 from "../../images/website_semester-project1.png";
+import Screenshot2 from "../../images/website-catfacts.png";
 import Screenshot3 from "../../images/website_letha.png";
 import DomButton from "../buttons/DomButton";
-import ProjectButton from "../buttons/ProjectButton";
+import ReactButton from "../buttons/ReactButton";
 import LethaButton from "../buttons/LethaButton";
 import styles from "./StyledCard.module.css";
 
@@ -41,35 +41,26 @@ function MyFrontendCard() {
 
       <div className={`${styles.item} ${styles.Card}`}>
         <div>
-          <h2 className="h2">SEMESTER PROJECT 1:</h2>
+          <h2 className="h2">REACT PROJECT</h2>
         </div>
 
         <div>
           <img src={Screenshot2} alt="Screenshot" />
         </div>
-
         <div>
           <p>
-            An interactive science museum called the Community Science Museum is
-            opening in your town/city. Its core target audience is primary and
-            middle school children (7-15) and families with young children. The
-            museum wants a website that is informative and appealing, that will
-            attract both middle school pupils and their parents. The site should
-            appeal to youngsters without pandering; it should take for granted
-            that the audience is inquisitive and intelligent. The website should
-            be informative and engaging, but should encourage viewers to visit
-            the museum itself. The website should be responsive and be easy to
-            use on a variety of devices.
+            Make a component that fetches and displays random cat facts from an
+            API.
           </p>
         </div>
         <Link
           className={styles.link}
-          to={"https://github.com/Silje32/Semester_project.git"}
+          to={"https://github.com/Silje32/CatFacts.git"}
         >
           VIEW GITHUB REPOSITORY
         </Link>
         <div>
-          <ProjectButton />
+          <ReactButton />
         </div>
       </div>
 
