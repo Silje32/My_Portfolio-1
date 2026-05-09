bab28a594a3feeea0a95b9b7b99ff54a73b713db
Silje32 <msh070425@gmail.com>
2026-05-09 14:03:15 +0200

    Added My Skills

--- src/components/cards/MyAboutCard.jsx
+++ src/components/cards/MyAboutCard.jsx
@@ -8,6 +8,12 @@ function MyAboutCard() {
         <p>I´m Silje Heldal Monsen</p>
         <p>I´m a fullstack developer who can turn your ideas into reality.</p>
       </div>
+
+      <div className={`${styles.item} ${styles.Card}`}>
+        <div>
+          <h2 className="h2">MY SKILLS</h2>
+        </div>
+      </div>
     </div>
   );
 }
