15e265c3af1bb52a058b0c9ade3f296fc0e5651c
Silje32 <msh070425@gmail.com>
2026-05-09 13:29:33 +0200

    Fixed a bug

--- src/components/cards/MyAboutCard.jsx
+++ src/components/cards/MyAboutCard.jsx
@@ -3,12 +3,12 @@ import styles from "./StyledCard.module.css";
 function MyAboutCard() {
   return (
     <div className={styles.container}>
-         <div className={`${styles.item} ${styles.Card}`}></div>
-      <h2>About Me</h2>
-      <p>Hi!</p>
-      <p>I´m Silje Heldal Monsen</p>
-      <p>I´m a fullstack developer who can turn your ideas into reality.</p>
-    </div>
+      <div className={`${styles.item} ${styles.Card}`}>
+        <h2>About Me</h2>
+        <p>Hi!</p>
+        <p>I´m Silje Heldal Monsen</p>
+        <p>I´m a fullstack developer who can turn your ideas into reality.</p>
+      </div>
     </div>
   );
 }
